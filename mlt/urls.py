cf2a437380b230ed6229400b07d61ac038243a2e
Jonny Gerig Meyer <jonny@oddbird.net>
2011-05-12 15:22:12 -0400

    initial html templates

--- mlt/urls.py
+++ mlt/urls.py
@@ -1,15 +1,23 @@
 from django.conf.urls.defaults import patterns, url, include
 
 from django.contrib import admin
-    
+
 admin.autodiscover()
-        
+
 
 
 urlpatterns = patterns(
     "",
-    url("^$", "django.views.generic.simple.direct_to_template", 
+    url("^$", "django.views.generic.simple.direct_to_template",
         {"template": "home.html"}, name="home"),
     url(r"^admin/", include(admin.site.urls)),
+
+    # wireframes
+    url(r"^login/$",
+        "django.views.generic.simple.direct_to_template",
+        {"template": "account/login.html"}),
+    url(r"^history/$",
+        "django.views.generic.simple.direct_to_template",
+        {"template": "history/history.html"}),
+
 )
-        
