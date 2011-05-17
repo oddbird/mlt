b563d9b32b459440fcfbbff54e6be90b93a16379
Jonny Gerig Meyer <jonny@oddbird.net>
2011-05-17 12:33:10 -0400

    added html5accordion (not active) and filter js

--- static/js/local.js
+++ static/js/local.js
@@ -1,4 +1,12 @@
 $(function() {
     $('#hcard-client-name .email').defuscate();
     $('input[placeholder], textarea[placeholder]').placeholder();
+    $('#filter .toggle a').click(
+        function() {
+            $('#filter .visual').toggleClass('compact').toggleClass('expanded');
+            return false;
+        }
+    );
+    // $('details').html5accordion('summary');
+    // $('.details').html5accordion('.summary');
 });
