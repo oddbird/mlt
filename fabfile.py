21747f327a6caa54e7f11046dbf2b6df0b23d5f2
Carl Meyer <carl@oddbird.net>
2011-05-30 15:11:49 -0500

    Moved compress cache into subdirectory of writable storage.

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,8 @@
-from fabric.api import *
+from fabric.api import local
 
 def deploy():
     local("./manage.py collectstatic --noinput")
     local("epio upload")
+    # ensure the compress cache dir exists
+    local("epio run_command mkdir -- -p ../data/compress-cache")
     local("epio django compress")
