9f752f5b4d3c93fd07cb9632ebc3db8becd502f5
Jonny Gerig Meyer <jonny@oddbird.net>
2011-06-02 14:21:29 -0400

    commented out unused js

--- static/js/local.js
+++ static/js/local.js
@@ -34,15 +34,15 @@
     $(function() {
         $('#hcard-client-name .email').defuscate();
         $('input[placeholder], textarea[placeholder]').placeholder();
-        $('#filter .toggle a').click(
-            function() {
-                $('#filter .visual').toggleClass('compact').toggleClass('expanded');
-                return false;
-            }
-        );
-        if ($('html').hasClass('no-details')) {
-            $('details').html5accordion('summary');
-        }
+        // $('#filter .toggle a').click(
+        //     function() {
+        //         $('#filter .visual').toggleClass('compact').toggleClass('expanded');
+        //         return false;
+        //     }
+        // );
+        // if ($('html').hasClass('no-details')) {
+        //     $('details').html5accordion('summary');
+        // }
         $('.details:not(html)').html5accordion('.summary');
         addressListHeight();
         lightboxBootstrap();
