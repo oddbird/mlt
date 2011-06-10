0647014daa8e391979b28164e6c27f884789c53f
Carl Meyer <carl@oddbird.net>
2011-06-10 14:16:11 -0500

    Remove placeholders from all account forms.

--- mlt/account/forms.py
+++ mlt/account/forms.py
@@ -2,37 +2,32 @@ from django.contrib.auth import forms as auth_forms
 
 import floppyforms as forms
 
-from ..core.forms import (
-    LabelPlaceholdersMixin, FloppyWidgetsMixin, NonFieldErrorsMixin)
+from ..core.forms import FloppyWidgetsMixin, NonFieldErrorsMixin
 
 
 
-class AuthenticationForm(LabelPlaceholdersMixin,
-                         FloppyWidgetsMixin,
+class AuthenticationForm(FloppyWidgetsMixin,
                          NonFieldErrorsMixin,
                          auth_forms.AuthenticationForm):
     pass
 
 
 
-class PasswordResetForm(LabelPlaceholdersMixin,
-                        FloppyWidgetsMixin,
+class PasswordResetForm(FloppyWidgetsMixin,
                         NonFieldErrorsMixin,
                         auth_forms.PasswordResetForm):
     widget_classes = {"email": forms.EmailInput}
 
 
 
-class SetPasswordForm(LabelPlaceholdersMixin,
-                      FloppyWidgetsMixin,
+class SetPasswordForm(FloppyWidgetsMixin,
                       NonFieldErrorsMixin,
                       auth_forms.SetPasswordForm):
     pass
 
 
 
-class PasswordChangeForm(LabelPlaceholdersMixin,
-                         FloppyWidgetsMixin,
+class PasswordChangeForm(FloppyWidgetsMixin,
                          NonFieldErrorsMixin,
                          auth_forms.PasswordChangeForm):
     pass
