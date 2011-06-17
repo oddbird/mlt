6192318f08573c6a5cd72a0f2c9b4b9611934dfc
Carl Meyer <carl@oddbird.net>
2011-06-17 10:57:31 -0500

    Fixed deploy migration command.

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,7 @@ from fabric.api import local
 def deploy():
     local("./manage.py collectstatic --noinput")
     local("epio upload")
-    local("epio migrate")
+    local("epio django migrate")
     # ensure the compress cache dir exists
     local("epio run_command mkdir -- -p ../data/compress-cache")
     local("epio django compress")
