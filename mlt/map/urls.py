e913fc4d5b1a3459296daa962785f39295e6f551
Carl Meyer <carl@oddbird.net>
2011-06-17 10:54:33 -0500

    No need to transform srid on every query anymore; cleanup.

--- mlt/map/urls.py
+++ mlt/map/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns(
     "mlt.map.views",
 
-    url(r"^geojson/(?P<westlat>[-.0-9]+)/(?P<eastlat>[-.0-9]+)/(?P<southlon>[-.0-9]+)/(?P<northlon>[-.0-9]+)/$", "geojson", name="map_geojson"),
+    url(r"^geojson/(?P<westlat>[-.0-9]+)/(?P<eastlat>[-.0-9]+)/(?P<southlng>[-.0-9]+)/(?P<northlng>[-.0-9]+)/$", "geojson", name="map_geojson"),
     )
