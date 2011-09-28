405d7698d740a0db2056595571cfb5da8958977e
Jonny Gerig Meyer <jonny@oddbird.net>
2011-09-28 14:39:51 -0400

    added explicit dismiss link for messages

--- static/js/local.js
+++ static/js/local.js
@@ -1310,10 +1310,7 @@ var MLT = MLT || {};
         $('#hcard-client-name .email').defuscate();
         $('input[placeholder], textarea[placeholder]').placeholder();
         $('.details:not(html)').html5accordion();
-        $('#messages').messages({
-            handleAjax: true,
-            closeLink: '.message'
-        });
+        $('#messages').messages({ handleAjax: true });
         addressListHeight();
         $('#addresstable .managelist .address .content .details .summary').live('click', function () {
             $(this).blur();
