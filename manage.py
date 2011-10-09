4a509a1023c36f256f4e3c28029a87d45ecf1450
Carl Meyer <carl@oddbird.net>
2011-10-08 22:13:51 -0600

    Removed unneeded sys.path addition.

--- manage.py
+++ manage.py
@@ -8,8 +8,6 @@ default manage.py.
 """
 import os, sys
 
-sys.path.insert(0, os.path.dirname(__file__))
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mlt.settings.base")
 
 from django.core.management import execute_from_command_line
