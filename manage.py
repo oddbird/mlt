2c8dc67ea3f243d728ea75e8e63fd8f66d37ae9a
Carl Meyer <carl@oddbird.net>
2011-10-08 22:16:22 -0600

    Check in manage.py that we're running as main script before doing stuff.

--- manage.py
+++ manage.py
@@ -8,8 +8,9 @@ default manage.py.
 """
 import os, sys
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mlt.settings.base")
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mlt.settings.base")
 
-from django.core.management import execute_from_command_line
+    from django.core.management import execute_from_command_line
 
-execute_from_command_line(sys.argv)
+    execute_from_command_line(sys.argv)
